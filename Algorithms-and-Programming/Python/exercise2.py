77b0294c38ae343484d3ba258fff9d3c62b89f7f
IFlickerz <nicolasmossmannlemos@gmail.com>
2023-03-06 05:53:21 -0300

    Second commit: adding exercises 2 and 3

---
+++ Algorithms-and-Programming/Python/exercise2.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print("Nome: " + nome + " - Idade:", idade)
---
+++ Algorithms-and-Programming/Python/exercise3.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+
+print("Nome:",  nome)
+
+altura = input("Digite sua altura: ")
+
+print("Altura:", altura)
+print("Obrigado!")
